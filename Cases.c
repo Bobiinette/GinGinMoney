66941552e75936be417a41a7964c23a0a66cc36c
Froufette <elena.bouchaud@gmail.com>
2017-02-20 17:44:36 +0100

    ajout de la documentation

--- Cases.c
+++ Cases.c
@@ -2,38 +2,76 @@
 #include <stdlib.h>
 #include "Cases.h"
 
+/*! \class t_listeCase Cases.h "inc/Cases.h"
+ *  \brief Structure de liste de cases.
+ */
 typedef struct t_listeCase {
   Case *c;
   struct t_listeCase *suivant;
 } CelluleCase, *ListeCase;
 
+/*! \fn ListeCase initListeCase(void)
+ *  \brief initialise une liste de cases.
+ *  \param ne prend aucun paramètre.
+ *  \return renvoie une liste vide.
+ */
 ListeCase initListeCase(void){
   return NULL;
 }
 
+/*! \fn int testListeCaseVide(ListeCase l)
+ *  \brief test si la liste de case est vide ou non.
+ *  \param l une liste de cases.
+ *  \return 1 si la liste de cases est vide, 0 si elle ne l'est pas.
+ */
 int testListeCaseVide(ListeCase l){
   return (l==NULL);
 }
 
+/*! \fn ListeCase constructeurListeCase(Case *d, ListeCase l)
+ *  \brief une fonction qui permet de rajouter un élément en tête de la liste.
+ *  \param d un pointeur de case.
+ *  \param l une liste de cases.
+ *  \return renvoie la case qui a été ajouté et le reste de la liste.
+ */
 ListeCase constructeurListeCase(Case *d, ListeCase l){
   CelluleCase *e;
   e=(CelluleCase *)malloc(sizeof(CelluleCase));
   e->c=d;
   e->suivant=l;
+  return e;
 }
 
+/*! \fn Case* getValeurListeCase(ListeCase l)
+ *  \param l une liste de cases.
+ *  \return le premier élément de la liste.
+ */
 Case* getValeurListeCase(ListeCase l){
   return l->c;
 }
 
+/*! \fn ListeCase getSuivantListeCase(ListeCase l)
+ *  \param l une liste de cases.
+ *  \return la liste sans son premier élément.
+ */
 ListeCase getSuivantListeCase(ListeCase l){
   return l->suivant;
 }
 
+/*! \fn void destructeurCelluleListeCase(CelluleCase *c)
+ *  \brief une fonction qui libère l'espace mémoire associée à une cellule.
+ *  \param c un pointeur d'une cellule de case
+ *  \return ne renvoie rien.
+ */
 void destructeurCelluleListeCase(CelluleCase *c){
   free(c);
 }
 
+/*! \fn void destructeurListeCase(ListeCase l)
+ *  \brief une fonction qui permet de libérer l'espace mémoire allouée d'une liste de cases.
+ *  \param l une liste de cases.
+ *  \return ne renvoie rien.
+ */
 void destructeurListeCase(ListeCase l){
   ListeCase ret;
   while(!testListeCaseVide(l)){
@@ -43,6 +81,12 @@ void destructeurListeCase(ListeCase l){
   }
 }
 
+/*! \fn int estPresentDansListeCase(Case *c, ListeCase l)
+ *  \brief une fonction qui cherche dans une liste de cases si une case est présente ou non.
+ *  \param c un pointeur de case.
+ *  \param l une liste de cases.
+ *  \return 1 si la case cherchée est dans la liste, 0 sinon.
+ */
 int estPresentDansListeCase(Case *c, ListeCase l){
   int res=0;
   while(!testListeCaseVide(l)){
@@ -56,6 +100,12 @@ int estPresentDansListeCase(Case *c, ListeCase l){
   return res;
 }
 
+/*! \fn ListeCase concatenationListeCase(ListeCase l, ListeCase m)
+ *  \brief une fonction qui permet de concaténer deux listes de cases.
+ *  \param l une liste de cases.
+ *  \param m une liste de cases.
+ *  \return la concatenation de deux listes.
+ */
 ListeCase concatenationListeCase(ListeCase l, ListeCase m){
   ListeCase temporaire = l;
   if(testListeCaseVide(l)){
@@ -66,12 +116,18 @@ ListeCase concatenationListeCase(ListeCase l, ListeCase m){
     while(l->suivant!=NULL){
       l=l->suivant;
     }
-    m=l->suivant;
+    l->suivant=m;
   }
   l = temporaire;
   return l;
 }
 
+/*! \fn void supprimeElementListeCase(Case *c, ListeCase *l)
+ *  \brief une fonction qui permet de supprimer un élément d'une liste et de libérer la mémoire qui y est allouée.
+ *  \param c un pointeur de case.
+ *  \param l un pointeur de liste.
+ *  \return la liste moins l'élément qui a été supprimé.
+ */
 void supprimeElementListeCase(Case *c, ListeCase *l){
   ListeCase temp = *l;
   ListeCase nouveauSuivant = initListeCase();
